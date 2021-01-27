0020f672e55f6dc17e30e04690b4866f84c41389
MatheusKael <matheuskael.silva@gmail.com>
2021-01-26 20:45:41 -0400

    Feat: Lazy load of components

---
+++ src/pages/catalog/products/[slug].tsx
@@ -0,0 +1,27 @@
+import { useRouter } from "next/router";
+import { useCallback, useState } from "react";
+import dynamic from "next/dynamic";
+
+const AddToCart = dynamic(() => import("../../../components/AddToCartModal"), {
+  loading: () => <p>Loading...</p>,
+  ssr: false,
+});
+
+export default function Product() {
+  const router = useRouter();
+  const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false);
+
+  const handleAddToCart = useCallback(() => {
+    setIsAddToCartModalVisible(true);
+  }, [setIsAddToCartModalVisible]);
+
+  return (
+    <div>
+      <h1>{router.query.slug}</h1>
+
+      <button onClick={handleAddToCart}>Add to cart</button>
+
+      {isAddToCartModalVisible && <AddToCart />}
+    </div>
+  );
+}
